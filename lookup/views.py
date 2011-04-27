db3781296bdc0b510d3c6ff3ae34039566cae6d0
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-04-27 21:54:30 +0000

    Added get_reference_names and get_mapping_names services. Started get_mapping_data and get_reference_data services.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@6 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- lookup/views.py
+++ lookup/views.py
@@ -7,6 +7,28 @@ import crds.rmap as rmap
 def get_best_refs(request, observatory, header):
     return rmap.get_best_refs(header, observatory)
 
+@jsonrpc_method('get_mapping_names(String)')
+def get_mapping_names(request, context):
+    ctx = rmap.get_pipeline_context("hst", context)
+    return ctx.mapping_names()
+
+@jsonrpc_method('get_reference_names(String)')
+def get_reference_names(request, context):
+    ctx = rmap.get_pipeline_context("hst", context)
+    return ctx.reference_names()
+
+@jsonrpc_method('get_mapping_data(String, String)')
+def get_mapping_data(request, context, mapping):
+    ctx = rmap.get_pipeline_context("hst", context)
+    filepath = ctx.locate_mapping(mapping)
+    return open(filepath).read()
+
+@jsonrpc_method('get_reference_data(String, String)')
+def get_reference_data(request, context, reference):
+    ctx = rmap.get_pipeline_context("hst", context)
+    filepath = ctx.locate_reference(reference)
+    return open(filepath).read()
+
 #@jsonrpc_method('lookup.sayHello')
 #def whats_the_time(request, name='Lester'):
 #  return "Hello %s" % name
