f5b514cdf2eea97c3dca8217b858523b15f6b9a9
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-05-06 14:11:15 +0000

    Renamed crds.rmap.get_pipeline_context to get_cached_mapping
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@11 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- lookup/views.py
+++ lookup/views.py
@@ -10,35 +10,35 @@ def get_best_refs(request, context, header):
 
 @jsonrpc_method('get_mapping_names(String)')
 def get_mapping_names(request, context):
-    ctx = rmap.get_pipeline_context(context)
+    ctx = rmap.get_cached_mapping(context)
     return ctx.mapping_names()
 
 @jsonrpc_method('get_reference_names(String)')
 def get_reference_names(request, context):
-    ctx = rmap.get_pipeline_context(context)
+    ctx = rmap.get_cached_mapping(context)
     return ctx.reference_names()
 
 @jsonrpc_method('get_mapping_data(String, String)')
 def get_mapping_data(request, context, mapping):
-    ctx = rmap.get_pipeline_context(context)
+    ctx = rmap.get_cached_mapping(context)
     filepath = ctx.locate.locate_mapping(mapping)
     return open(filepath).read()
 
 @jsonrpc_method('get_mapping_url(String, String)')
 def get_mapping_url(request, context, mapping):
-    ctx = rmap.get_pipeline_context(context)
+    ctx = rmap.get_cached_mapping(context)
     return ctx.locate.mapping_url("http://" + request.get_host(), mapping)
 
 @jsonrpc_method('get_reference_data(String, String)')
 def get_reference_data(request, context, reference):
-    ctx = rmap.get_pipeline_context(context)
+    ctx = rmap.get_cached_mapping(context)
     filepath = ctx.locate.locate_reference(reference)
     refdata = open(filepath).read()
     return base64.b64encode(refdata)
 
 @jsonrpc_method('get_reference_url(String, String)')
 def get_reference_url(request, context, reference):
-    ctx = rmap.get_pipeline_context(context)
+    ctx = rmap.get_cached_mapping(context)
     return ctx.locate.reference_url("http://" + request.get_host(), reference)
 
 #@jsonrpc_method('lookup.sayHello')
