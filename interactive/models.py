fdbb16b159a357c847063f68ef7d8926588bd7b6
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-07-20 16:28:36 +0000

    Added basic data blob model to interactive server.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@29 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- interactive/models.py
+++ interactive/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+from crds.compat import literal_eval
+
+class DataBlob(models.Model):
+    """A generic data format based which lets us transactionally store anything
+    which has a repr() which can be literal_eval()'ed back to life.
+    """
+    kind  = models.CharField(
+            max_length = 64,
+            help_text = "string identifying the type of this blob")
+
+    blob  = models.TextField( 
+            help_text  = "repr() of dict with blob contents")
+    
+    def freeze(self, contents):
+        self.blob = repr(contents)
+        self.save()
+    
+    def thaw(self):
+        return literal_eval(self.blob)
