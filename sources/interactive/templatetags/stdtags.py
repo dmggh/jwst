bc4c62d2f11b47eab4328077faaef12d4fd7df5c
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-08-12 14:13:35 +0000

    More installation,  including sibling install of ../CRDS/{lib,hst,client} dev-mode server appears to be working
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@58 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/interactive/templatetags/stdtags.py
+++ sources/interactive/templatetags/stdtags.py
@@ -6,9 +6,6 @@ import sys
 
 from django import template
 
-import delivery.common as common
-import delivery.common.widgets as widgets
-import delivery.common.logging as logging
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
@@ -20,173 +17,21 @@ def grey(value):
     return mark_safe("<span class='grey'>" + value + "</span>")
 grey.is_safe = True
 
-# ==========================================================
-def context_eval(context, expr):
-    """Evaluate a template expression in a context and
-    return the string result.
-    """
-    u = template.Template("{{ " + expr + " }}")
-    return u.render(context)
+@register.filter(name='green')
+@stringfilter
+def green(value):
+    return mark_safe("<span class='green'>" + value + "</span>")
+green.is_safe = True
 
-# ==========================================================
-# {% form_field <formname>.<fieldname> %}
+@register.filter(name='blue')
+@stringfilter
+def blue(value):
+    return mark_safe("<span class='blue'>" + value + "</span>")
+blue.is_safe = True
 
-class FormField(template.Node):
-    def __init__(self, formname, fieldname):
-        self._form = formname
-        self._field = fieldname
-
-    def render(self, context):
-        return common.widgets.render_html_field(
-            context[self._form], self._field)
-
-def form_field(parser, token):
-    """{% form_field <formname>.<fieldname> %}"""
-    tag_name, formfield = token.split_contents()
-    form, field = formfield.split(".")
-    return FormField(form, field)
-
-register.tag("form_field", form_field)
-
-# ==========================================================
-# {% select <index_expr> <choices>... %}
-
-class SelectNode(template.Node):
-    def __init__(self, tag, selector, *choices):
-        self._tag = tag
-        self._selector = selector
-        self._choices = choices
-
-    def render(self, context):
-        selector = context_eval(context, self._selector)
-        try:
-            which = int(selector)
-        except:
-            if selector == "False":
-                which = 0
-            elif selector == "True":
-                which = 1
-            else:
-                raise
-        # logging.log("select:",selector,which,self._choices)
-
-        return self._choices[which]
-
-def select(parser, token):
-    """{% select <index_var> <choices>... %}"""
-    args = token.split_contents()
-    return SelectNode(*args)
-
-register.tag("select", select)
-
-# ==========================================================
-# {% button_with_help <tag> <name> <url> <help_text> %}
-
-def remove_quotes(s):
-    start = s[0] in ["'",'"'] and 1 or 0
-    end   = s[-1] in ["'",'"'] and -1 or len(s)
-    return s[start:end]
-
-class ButtonWithHelp(template.Node):
-    def __init__(self, tag, name, url, help_text):
-        self._tag = tag
-        self._name = remove_quotes(name)
-        self._url = remove_quotes(url)
-        self._help_text = remove_quotes(help_text)
-
-    def render(self, context):
-        t = template.loader.get_template("help_button.html")
-        for var in ["name","url","help_text"]:
-            u = template.Template(getattr(self, "_"+var))
-            context[var] = u.render(context)
-        context["balloon_id"] = widgets.get_balloon_id()
-        return t.render(context)  # HTML
-
-def button_with_help(parser, token):
-    """{% button_with_help <name> <url> <help_text> %}"""
-    return ButtonWithHelp(*token.split_contents())
-
-register.tag("button_with_help", button_with_help)
-
-# ==========================================================
-# {% label_with_help <label> <help_text> %}
-
-class LabelWithHelp(template.Node):
-    def __init__(self, tag, label, help_text):
-        self._tag = tag
-        self._label = remove_quotes(label)
-        self._help_text = remove_quotes(help_text)
-
-    def render(self, context):
-        t = template.loader.get_template("help_label.html")
-        for var in ["label","help_text"]:
-            u = template.Template(getattr(self, "_"+var))
-            context[var] = u.render(context)
-        context["balloon_id"] = widgets.get_balloon_id()
-        return t.render(context)  # HTML
-
-def label_with_help(parser, token):
-    """{% label_with_help <name> <help_text> %}"""
-    return LabelWithHelp(*token.split_contents())
-
-register.tag("label_with_help", label_with_help)
-
-# ==========================================================
-# {% submit_button <tag> <name> <help_text> %}
-
-def remove_quotes(s):
-    start = s[0] in ["'",'"'] and 1 or 0
-    end   = s[-1] in ["'",'"'] and -1 or len(s)
-    return s[start:end]
-
-class SubmitButtonWithHelp(template.Node):
-    def __init__(self, tag, name, help_text):
-        self._tag = tag
-        self._name = remove_quotes(name)
-        self._help_text = remove_quotes(help_text)
-
-    def render(self, context):
-        t = template.loader.get_template("submit_button.html")
-        for var in ["name","help_text"]:
-            u = template.Template(getattr(self, "_"+var))
-            context[var] = u.render(context)
-        context["balloon_id"] = widgets.get_balloon_id()
-        return t.render(context)  # HTML
-
-def submit_button(parser, token):
-    """{% submit_button <tag> <name> <help_text> %}"""
-    return SubmitButtonWithHelp(*token.split_contents())
-
-register.tag("submit_button", submit_button)
-
-# ==========================================================
-
-def do_button_table(parser, token):
-    """
-    {% button_table attributes... %}
-       {% button_with_help ... %}
-       {% button_with_help ... %}
-       {% button_with_help ... %}
-       ...
-    {% endbutton_table %}
-    """
-    nodelist = parser.parse(('endbutton_table',))
-    parser.delete_first_token()
-    return ButtonTable(nodelist)
-
-class ButtonTable(template.Node):
-    def __init__(self, nodelist):
-        self.nodelist = nodelist
-
-    def render(self, context):
-        buttons = []
-        for n in self.nodelist:
-            rendered = n.render(context)
-            if rendered.strip():
-                buttons.append(rendered)
-        context["buttons"] = buttons
-        t = template.loader.get_template("button_table.html")
-        return t.render(context)  # HTML
-
-register.tag("button_table", do_button_table)
+@register.filter(name='red')
+@stringfilter
+def red(value):
+    return mark_safe("<span class='red'>" + value + "</span>")
+red.is_safe = True
 
--- sources/urls.py
+++ sources/urls.py
@@ -11,21 +11,21 @@ urlpatterns = patterns('',
   url(r'^json/', jsonrpc_site.dispatch, name="jsonrpc_mountpoint"),
   url(r'^json/(?P<method>[a-zA-Z0-9.]+)$', jsonrpc_site.dispatch), # for HTTP GET only, also omissible
   
-  url(r'^$', 'interactive.views.index'),          
+  url(r'^$', 'crds.server.interactive.views.index'),          
 
   url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),          
-  url(r'^logout/$', 'interactive.views.logout'),          
+  url(r'^logout/$', 'crds.server.interactive.views.logout'),          
 
-  url(r'^bestrefs/$', 'interactive.views.bestrefs_index'),          
-  url(r'^bestrefs/input/$', 'interactive.views.bestrefs_input'),          
-  url(r'^bestrefs/compute/$', 'interactive.views.bestrefs_compute'),
+  url(r'^bestrefs/$', 'crds.server.interactive.views.bestrefs_index'),          
+  url(r'^bestrefs/input/$', 'crds.server.interactive.views.bestrefs_input'),          
+  url(r'^bestrefs/compute/$', 'crds.server.interactive.views.bestrefs_compute'),
 
-  url(r'^submit/$', 'interactive.views.submit_file'),          
-  url(r'^blacklist/$', 'interactive.views.blacklist_file'),          
-  url(r'^using/$', 'interactive.views.using_file'),          
-  url(r'^certify/$', 'interactive.views.certify_file'),          
-  url(r'^difference/$', 'interactive.views.difference_files'),
-  url(r'^browse/$', 'interactive.views.browse_files')        
+  url(r'^submit/$', 'crds.server.interactive.views.submit_file'),          
+  url(r'^blacklist/$', 'crds.server.interactive.views.blacklist_file'),          
+  url(r'^using/$', 'crds.server.interactive.views.using_file'),          
+  url(r'^certify/$', 'crds.server.interactive.views.certify_file'),          
+  url(r'^difference/$', 'crds.server.interactive.views.difference_files'),
+  url(r'^browse/$', 'crds.server.interactive.views.browse_files')        
           
 #        (r'site_media/(?P<path>.*)$', 'django.views.static.serve',
 #         {'document_root': settings.MEDIA_ROOT }),
