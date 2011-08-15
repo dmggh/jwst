ad275768eab28df508a996ee3449aeecf5681186
stsci_jmiller <stsci_jmiller@stsci.edu>
2011-08-15 14:29:54 +0000

    First setup for apache/mod_python on etcbrady (pyetc test machine), operational not fully tested
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@66 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/configs/config.test.py
+++ sources/configs/config.test.py
@@ -1,6 +1,12 @@
 import os
 
+install_dir='/data1/CRDS/CRDS_server/webserver'
+
+CRDS_REFERENCE_URL = "ftp://ftp.stsci.edu/cdbs/"   # .e.g http://crds_refs.stsci.edu
+CRDS_MAPPING_URL =   "http://etcbrady.stsci.edu:4997"
+
 servertype = 'mod_python'
+apachemod = "/data1/pyetc_third_party_4/lib/apache"
 port = 4997
 
 debug = True
@@ -11,10 +17,4 @@ data_dir = install_dir+"/data"
 PYSYN_CDBS = ""
 
 telescopes = [ 'hst' ]
-
-install_dir='/data1/CRDS/CRDS_server/webserver'
-
-CRDS_REFERENCE_URL = "ftp://ftp.stsci.edu/cdbs/"   # .e.g http://crds_refs.stsci.edu
-
-CRDS_MAPPING_URL =   "http://etcbrady.stsci.edu:4997"
 
