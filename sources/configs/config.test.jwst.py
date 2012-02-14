1bd2911cd13ae590e5c6970c83828d9e7653b914
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-02-14 16:09:12 +0000

    Added JWST test config module.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@340 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

---
+++ sources/configs/config.test.jwst.py
@@ -0,0 +1,22 @@
+import os
+
+install_dir='/data1/CRDS/CRDS_jwst/webserver'
+
+servertype = 'mod_python'
+apachemod = "/data1/pyetc_third_party_4/lib/apache"
+port = 4995
+
+debug = True
+dbtype='sqlite'
+table_prefix = 'crds_jw_'
+data_dir = install_dir+"/data"
+
+PYSYN_CDBS = ""
+
+telescopes = [ 'jwst' ]
+
+CRDS_URL = "http://etcbrady.stsci.edu:4995"
+
+CRDS_REFERENCE_URL = CRDS_URL + "/get/" 
+CRDS_MAPPING_URL   = CRDS_URL + "/get/" 
+
