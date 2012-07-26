61269690daf54cf1d9f4ba21e9c2c938bd7c9e88
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-07-26 14:32:56 +0000

    Added --super-user flag to tools/adduser.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@480 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- tools/adduser.py
+++ tools/adduser.py
@@ -2,19 +2,28 @@ import sys
 
 from django.contrib.auth.models import User
 
-def adduser(user, email, password):
+def adduser(user, email, password, super_user=False):
     user = User.objects.create_user(user, email, password)
 
     # At this point, user is a User object that has already been saved
     # to the database. You can continue to change its attributes
     # if you want to change other fields.
     user.is_staff = True
+    if super_user:
+        user.is_superuser = True
     user.save()
 
 if __name__ == "__main__":
+    
+    if "--super-user" in sys.argv:
+        sys.argv.remove("--super-user")
+        super_user = True
+    else:
+        super_user = False
+        
     if len(sys.argv) != 4:
         print >>sys.stderr, "usage: adduser.py <username> <email> <password>"
         sys.exit(-1)
     else:
-        adduser(*sys.argv[1:])
+        adduser(*sys.argv[1:], super_user=super_user)
 
