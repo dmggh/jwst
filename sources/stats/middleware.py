ca2674f7e33ef1332e28cbe44053b2e2dfb57355
stsci_jmiller <stsci_jmiller@stsci.edu>
2012-08-06 16:11:26 +0000

    Lots more work on logging middleware admin display,  left with request-only logging for now.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@500 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/stats/middleware.py
+++ sources/stats/middleware.py
@@ -1,7 +1,34 @@
+
 import crds.server.stats.models as smodels
 
 class LogMiddleware(object):
     
     def process_request(self, request):
-        smodels.LogModel.log_request(request)
-        
+        try:
+            smodels.LogModel.log_request(request)
+        except smodels.SkipPathError:
+            pass
+        except Exception, exc:
+            print "LogMiddleware process_request failed:", str(exc)
+
+    def process_response(self, request, response):
+        return response    # XXX TODO disabled
+    
+        try:
+            smodels.LogModel.log_response(request, response)
+        except smodels.SkipPathError:
+            pass
+        except Exception, exc:
+            print "LogMiddleware process_response failed:", str(exc)
+        return response
+
+    def process_exception(self, request, exception):
+        return None    # XXX TODO disabled
+    
+        try:
+            smodels.LogModel.log_exception(request, exception)
+        except smodels.SkipPathError:
+            pass
+        except Exception, exc:
+            print "LogMiddleware process_exception failed:", str(exc)
+            
--- sources/stats/models.py
+++ sources/stats/models.py
@@ -7,6 +7,12 @@ from django.db import models
 from crds import timestamp, compat
 from crds.server import config
 
+MAX_RESPONSE_LEN = 100000
+SKIP_PATHS = ('/admin',)
+
+class SkipPathError(Exception):
+    """Paths listed in SKIP_PATHS are not logged.   Instead this exception is raised."""
+    
 # Create your models here.
 
 def double(xs):
@@ -14,9 +20,10 @@ def double(xs):
 
 def make_dict(name, obj):
     try:
-        return { key:value for (key,value) in obj.items() }
+        return { str(key):str(value) for (key,value) in obj.items() }
     except:
         return "error capturing " + name
+    
 
 class LogModel(models.Model):
     """A generic model for logging stuff,  request, responses, and messages."""
@@ -25,19 +32,21 @@ class LogModel(models.Model):
         db_table = config.table_prefix + "_log" # rename SQL table from interactive_fileblob
         
     date = models.DateTimeField(auto_now_add=True)
-    event = models.CharField(max_length=32, choices=double(["request","response","log"]), default="log")
-    info = models.TextField()
+    event = models.CharField(max_length=32, 
+                             choices=double(["request","response","log", "exception"]), 
+                             default="log")
+    blob = models.TextField()
     
     @property
-    def liveinfo(self):
-        if not hasattr(self, "_liveinfo"):
-            self._liveinfo = compat.literal_eval(self.info)
-        return self._liveinfo
+    def liveblob(self):
+        if not hasattr(self, "_liveblob"):
+            self._liveblob = compat.literal_eval(self.blob)
+        return self._liveblob
         
     @property
     def jsonpars(self):
         if not hasattr(self, "_jsonpars"):
-            self._jsonpars = json.loads(self.liveinfo["POST"].keys()[0])
+            self._jsonpars = json.loads(self.liveblob["POST"].keys()[0])
         return self._jsonpars
 
     @property
@@ -45,51 +54,127 @@ class LogModel(models.Model):
         return "[" + timestamp.reformat_date(self.date)[:-4] + "]"
     
     def __unicode__(self):
-        if self.liveinfo["path"].startswith("/json"):
-            s = self.datestr + " " + self.liveinfo["host"] + " JSON " + \
-                self.jsonpars["method"] + " " + str(self.jsonpars["params"])
+        if self.event == "request":
+            return self.__unicode__request()
+        elif self.event == "response":
+            return self.__unicode__response()
+        elif self.event == "log":
+            return self.__unicode__log()
+        elif self.event == "exception":
+            return self.__unicode__exception()
         else:
-            s = self.datestr + " " + self.liveinfo["host"] + " " + self.liveinfo["method"] + \
-                 " " + self.liveinfo["path"]
-            if self.liveinfo["method"] == "GET":
-                s += " " + str(self.liveinfo["GET"])
+            return self.__unicode__prefix()
+
+    @property
+    def is_json(self):
+        return self.liveblob["path"].startswith("/json")
+
+    def __unicode__prefix(self):
+        event = { "request":"REQ", "response":"RESP", }[self.event]
+        s = self.datestr + " " + event.upper() + " " + \
+            self.liveblob["HTTP_HOST"] + "(" + self.liveblob["REMOTE_ADDR"] + ")"
+        if self.is_json:
+             s += " JSON " + self.jsonpars["method"] 
+        else:
+            s += " " + self.liveblob["method"] + " " + self.liveblob["path"]
+        return s
+        
+    def __unicode__request(self):
+        s = self.__unicode__prefix()
+        if self.is_json:
+            s += " " + str(self.jsonpars["params"])
+        else:
+            if self.liveblob["method"] == "GET":
+                s += " " + str(self.liveblob["GET"])
             else:
-                s += " " + str(self.liveinfo["PUT"])
+                s += " " + str(self.liveblob["POST"])
         return s
     
+    def __unicode__response(self):
+        return self.__unicode__prefix() + " " + str(self.liveblob["status_code"])
+
+    def __unicode__exception(self):
+        return self.__unicode__prefix() + " " + str(self.liveblob["exception_str"])
+
+    def __unicode__log(self):
+        return self.__unicode__prefix() + " " + str(self.liveblob["message"])
+
     def __repr__(self):
-        return self.datestr + " " + self.event + "\n" + pprint.pformat(eval(self.info))
+        return self.datestr + " " + self.event + "\n" + \
+            pprint.pformat(compat.literal_eval(self.blob))
 
     @classmethod
-    def log_string(cls, info, event="log"):
+    def log_string(cls, blob, event="log"):
         l = cls()
         l.event = event
-        l.info = info
+        l.blob = blob
         l.save()
+
+    @classmethod    
+    def stdblob(cls, request, **keys):
+        if request.path == "/" or request.path.startswith(SKIP_PATHS):
+            raise SkipPathError(request.path)
+        
+        get = make_dict("GET", request.GET)
+        post = make_dict("POST", request.POST)
+        meta = make_dict("META", request.META)
+        
+        d = dict(
+                # REMOTE_HOST = meta.get("REMOTE_HOST", "unknown"),
+                REMOTE_ADDR = meta.get("REMOTE_ADDR", "unknown"),
+                HTTP_HOST = meta.get("HTTP_HOST", "unknown"),
+                HTTP_USER_AGENT = meta.get("HTTP_USER_AGENT", "unknown"),
+                HTTP_REFERRER = meta.get("HTTP_REFERRER", "unknown"),
+                user=str(request.user), 
+                method=request.method,
+                path=request.path,
+                GET=get, 
+                POST=post, 
+                )
+        d.update(**keys)
+        return d
         
     @classmethod
     def log_request(cls, request):
         meta = make_dict("META", request.META)
-        get = make_dict("GET", request.GET)
-        post = make_dict("POST", request.POST)
         cookies = make_dict("COOKIES", request.COOKIES)
-        files = [ (f.name, f.size) for f in request.FILES ]
-        d = dict(
-                 # META=meta, 
-                 GET=get, 
-                 POST=post, 
-                 FILES=files,
-                 COOKIES=cookies,
-                 method=request.method,
-                 path=request.path,
-                 full_path=request.get_full_path(),
-                 ajax=request.is_ajax(),
-                 user=str(request.user), 
-                 encoding=request.encoding,
-                 host=request.get_host(),
-                 secure=request.is_secure(),
-                 authenticated=request.user.is_authenticated())
+        files = [ (f.name, f.size) for f in request.FILES.values() ]
+        d = cls.stdblob(
+            request,
+            # META=meta, 
+            FILES=files,
+            COOKIES=cookies,
+            full_path=request.get_full_path(),
+            ajax=request.is_ajax(),
+            encoding=request.encoding,
+            secure=request.is_secure(),
+            authenticated=request.user.is_authenticated())
         cls.log_string(repr(d), event="request")
+
+    @classmethod
+    def log_response(cls, request, response):
+        d = cls.stdblob(request,
+                status_code = response.status_code,
+                ajax=request.is_ajax(),
+                encoding=request.encoding,
+                )
+        cls.log_string(repr(d), event="response")
+
+    @classmethod
+    def log_exception(cls, request, exception):
+        d = cls.stdblob(request,
+                exception_repr = repr(exception),
+                exception_str = str(exception),
+                )
+        cls.log_string(repr(d), event="exception")
+
+    @classmethod
+    def log_message(cls, request, message):
+        d = cls.stdblob(request)
+        d.update(dict(
+                message = message
+                ))
+        cls.log_string(repr(d), event="log")
 
     @classmethod
     def wipe(cls):
