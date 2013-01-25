eca32b8d4749abd9a9d1261eb57eb1abcc4c9a88
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-01-25 23:28:10 +0000

    Started command line program for file submission.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@706 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

---
+++ sources/interactive/submit.py
@@ -0,0 +1,50 @@
+from crds import cmdline
+
+class SubmitFilesScript(cmdline.Script):
+    """Command line version of CRDS file submissions."""
+    
+    description = """Submit files to CRDS relative to the specified context.
+    
+    There are two broad scenarios which have several use cases.
+    
+    I.  Submit primitive files.   
+    
+        Other than basic certification,  CRDS will accept the files as is.
+        CRDS does not attempt to interrelate these files.  The given context is
+        used to locate existing files which correspond to the submitted files
+        for differencing and certification,  particularly for references.
+    
+        a. Submit references, of any known instruments or types.
+
+        b. Submit .rmap's, .imap's, or .pmap's,  in any relation
+    
+    II. Submit files and generate mappings.
+    
+        CRDS accepts related primitive files, but then automatically generates
+        related higher level mapping files.
+        
+        a. Submit references for one instrument but possibly several types,  
+                and generate .rmap's, .imap, and .pmap.
+                
+        b. Submit .rmap's,  generate .imap's and .pmap's. 
+    """
+    
+    def add_args(self):
+        self.add_argument("files", nargs="+", help="Paths of references and mappings to add to CRDS.""")
+        self.add_argument("--derive-from-context", store_in="derive_from_context",
+                          help=".pmap used for certification or basis for new mappings/.pmap")
+        self.add_argument("--change-level", choices=["trivial","medium","severe"], store_in="change_level", default="severe",
+                          help="Degree to which submitted files impact science results.")
+        self.add_argument("--description", help="Free text description of file submission.")
+        self.add_argument("--auto-rename", action="store_true", default=False,
+                          help="If specified,  automatically rename files to CRDS-style versioned names.")
+        self.add_argument("--compare-old-references", action="store_true", default=False,
+                          help="If specified, check references against the files they replace in derive-from-context, where applicable.")
+        self.add_argument("--generate-rules", action="store_true", default=False,
+                          help="If specified, generate mappings as required to add the submitted files to the derived-from-context.")
+        
+    def main(self):
+        submit_files(self.args.derive_from_context, self.args.files)
+        
+if __name__ == "__main__":
+    NewContextScript()()
