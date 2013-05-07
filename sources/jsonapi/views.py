185d4e913fc4b7d4143b06e965160e9e4484aa39
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-05-07 14:24:39 +0000

    Relaxed .pmap/.imap only restriction on check_context in jsonapi,  allowing .rmaps as well.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@831 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/jsonapi/views.py
+++ sources/jsonapi/views.py
@@ -114,8 +114,8 @@ def check_context(context):
                                          " supported by this server.   Switch servers or contexts.") %
                                          (context, imodels.OBSERVATORY))
     blob = check_known_file(context)
-    if not rmap.is_mapping(context) or not context.endswith((".imap", ".pmap")):
-        raise UnknownContextError("Context parameter '%s' is not a known CRDS .pmap or .imap file." % context)
+    if not rmap.is_mapping(context):
+        raise UnknownContextError("Context parameter '%s' is not a known CRDS .pmap, .imap, or .rmap file." % context)
     return blob
 
 def check_mapping(mapping):
