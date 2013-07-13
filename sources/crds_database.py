1bf425d3dc934a803fe70cf2769fefd7031db3f6
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-07-13 19:21:00 +0000

    Removed site specific files which shouldn't have been committed.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@915 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/crds_database.py
+++
@@ -1,9 +0,0 @@
-import config
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-
-        'NAME': config.install_dir + '/python/lib/python/crds/server/sqlite3.db',
-    }
-}
