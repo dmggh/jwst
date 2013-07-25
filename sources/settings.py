804418351fcf13c01fe10fc4219747c7c4168836
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-07-25 12:40:36 +0000

    Set USE_X_FORWARDED_HOST = True in settings.py to solve:
    """
    Here's the Django message for the CSRF failure for all POSTs:
    
        Referer checking failed - https://hst-crds.stsci.edu/browse_db/ does not match https://plhstcrdsv1.stsci.edu:8001/.
    """
    
    
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@957 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/settings.py
+++ sources/settings.py
@@ -13,6 +13,7 @@ MANAGERS = ADMINS
 
 ALLOWED_HOSTS = ['*']
 
+USE_X_FORWARDED_HOST = True
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
