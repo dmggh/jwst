38a27b8eee990db813f12184e97e46ef43a1113d
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-08-02 00:20:34 +0000

    Re-enabled django.template.loaders.app_directories.Loader
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@998 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/settings.py
+++ sources/settings.py
@@ -80,7 +80,7 @@ LOGIN_URL = "/login/"
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
-    # 'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
 
