c3a4261716ab84ec617fe66ac21c53169b155b1d
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-08-25 11:42:02 +0000

    Fix server banner test not to display (production)
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@1042 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/interactive/views.py
+++ sources/interactive/views.py
@@ -285,7 +285,7 @@ def get_rendering_dict(request, dict_=None, requires_pmaps=False):
         "locked_instrument" : locked,
 
         "auto_rename" : False,
-        "server_usecase" : "(" + sconfig.server_usecase.lower() + ")" if sconfig.server_usecase != "prod" else "",
+        "server_usecase" : "(" + sconfig.server_usecase.lower() + ")" if not sconfig.server_usecase.startswith("prod") else "",
     }
     
     # echo escaped inputs.
