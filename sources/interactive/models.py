3370327638c421d3cfe21ffe64b9c38a963511c8
stsci_jmiller <stsci_jmiller@stsci.edu>
2013-11-16 17:55:31 +0000

    Finall version of AuditBlob modification with all fields now model fields.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@1216 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/interactive/models.py
+++ sources/interactive/models.py
@@ -1219,11 +1219,6 @@ class AuditBlob(BlobModel):
     class Meta:
         db_table = TABLE_PREFIX + "_actions" # rename SQL table from interactive_fileblob
         
-    model_fields = BlobModel.model_fields + ["user", "date", "action", "filename", "observatory", "instrument", "filekind",
-                                             "why", "details"]
-    repr_list = unicode_list = ["date", "filename", "action", "user", "instrument", "filekind", "why", "details"]
-    unicode_list = ["date", "action", "user", "filename", "why"]
-    
     user = models.CharField(max_length=64, default="", help_text="user who performed this action")
     date = models.CharField(max_length=26, default="", help_text="unique name of this model.")
     action = SimpleCharField( AUDITED_ACTIONS, "name of action performed", "" )
@@ -1234,18 +1229,24 @@ class AuditBlob(BlobModel):
     why = models.TextField(help_text="reason this action was performed", default="")
     details = models.TextField(help_text="supplementary info", default="")
 
+    model_fields = BlobModel.model_fields + ["user", "date", "action", "filename", "observatory", "instrument", "filekind",
+                                             "why", "details"]
+
     blob_fields = dict(
         # User supplied fields
-        user = BlobField(str, "user performing this action", ""),
-        date = BlobField(str, "date action performed", ""),
-        action = BlobField(AUDITED_ACTIONS,"name of action performed", ""),        
-        filename = BlobField("^[A-Za-z0-9_./]*$","file affected by this action", "None"),
-        observatory = BlobField(OBSERVATORIES, "associated observatory", ""),
-        instrument = BlobField(INSTRUMENTS + ["unknown"], "associated instrument", ""),
-        filekind = BlobField(FILEKINDS + ["unknown"], "associated filekind", ""),
-        why = BlobField(str, "reason this action was performed",""),
-        details = BlobField(str, "supplementary info", ""),
+        # user = BlobField(str, "user performing this action", ""),
+        # date = BlobField(str, "date action performed", ""),
+        # action = BlobField(AUDITED_ACTIONS,"name of action performed", ""),        
+        # filename = BlobField("^[A-Za-z0-9_./]*$","file affected by this action", "None"),
+        # observatory = BlobField(OBSERVATORIES, "associated observatory", ""),
+        # instrument = BlobField(INSTRUMENTS + ["unknown"], "associated instrument", ""),
+        # filekind = BlobField(FILEKINDS + ["unknown"], "associated filekind", ""),
+        # why = BlobField(str, "reason this action was performed",""),
+        # details = BlobField(str, "supplementary info", ""),
     )
+    
+    repr_list = unicode_list = ["date", "filename", "action", "user", "instrument", "filekind", "why", "details"]
+    unicode_list = ["date", "action", "user", "filename", "why", "details"]
     
     @classmethod
     def new(cls, user, action, affected_file, why, details, 
