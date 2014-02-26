d28bd9026cd6afd03474be2aa2c9641b53c760c6
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-26 21:26:56 +0000

    Fixed JWST ops download urls.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@1313 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/configs/config.ops.jwst.py
+++ sources/configs/config.ops.jwst.py
@@ -33,8 +33,8 @@ CRDS_REFERENCE_URL = CRDS_URL + "get/"
 CRDS_MAPPING_URL   = CRDS_URL + "get/"
 
 # These may get redirected to the archive or a static file server
-CRDS_UNCHECKED_REFERENCE_URL = CRDS_URL + "unchecked_get/references/hst/"
-CRDS_UNCHECKED_MAPPING_URL   = CRDS_URL + "unchecked_get/mappings/hst/"
+CRDS_UNCHECKED_REFERENCE_URL = CRDS_URL + "unchecked_get/references/" + observatory + "/"
+CRDS_UNCHECKED_MAPPING_URL   = CRDS_URL + "unchecked_get/mappings/" + observatory + "/"
 
 # These are the file states which are available for download or rpc.
 # XXX TODO restrict to archived or operational
