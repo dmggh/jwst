0d9541c9104b15f30d66f13511ed0b61c231c178
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-02-26 22:14:57 +0000

    Modified jsonapi exception class to make it easier to encode error strings to preclude XSS.  crds.client.api should undo any encoding prior to presenting error messages.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@1314 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/jsonapi/views.py
+++ sources/jsonapi/views.py
@@ -5,10 +5,12 @@ from __future__ import division
 import base64
 import math
 import re
-import zlib 
+import zlib
 
 from jsonrpc import jsonrpc_method
 from jsonrpc.exceptions import Error
+
+from django.utils import html
 
 from crds.server.interactive import models as imodels
 from crds.server.interactive import versions, database, crds_db
@@ -56,62 +58,75 @@ def create_unchecked_url(observatory, filename):
 
 # ===========================================================================
 
-class UnknownContextError(Error):
+class EncError(Error):
+    """HTML encode the error message to prevent script injection in error messages."""
+    def __init__(self, *args, **keys):
+        # self.__msg = html.format_html(*args, **keys)
+        self.__msg = args[0].format(*args[1:], **keys)
+        super(EncError, self).__init__(self.__msg)
+    
+    def __repr__(self):
+        return self.__class__.__name__ + "(" + repr(self.__msg) + ")"
+        
+    def __str__(self):
+        return self.__msg
+
+class UnknownContextError(EncError):
     """The specified context is not a known CRDS mapping."""
     
-class MismatchedContextError(Error):
+class MismatchedContextError(EncError):
     """The specified context's observatory doesn't match this server."""
     
-class UnknownMappingError(Error):
+class UnknownMappingError(EncError):
     """The specified mapping parameter is not a known CRDS mapping file."""
     
-class UnknownReferenceError(Error):
+class UnknownReferenceError(EncError):
     """The specified reference parameter is not a known CRDS reference file."""
     
-class InvalidHeaderError(Error):
+class InvalidHeaderError(EncError):
     """The specified header parameter is not suitable."""
 
-class InvalidObservatoryError(Error):
+class InvalidObservatoryError(EncError):
     """The specified observatory is not supported."""
     
-class InvalidInstrumentError(Error):
+class InvalidInstrumentError(EncError):
     """The specified instrument is not supported."""
     
-class InvalidReftypesError(Error):
+class InvalidReftypesError(EncError):
     """The specified reftypes is not a list of strings."""
 
-class UnknownFile(Error):
+class UnknownFile(EncError):
     """There's no record of a file in the CRDS database."""
 
-class UnavailableFile(Error):
+class UnavailableFile(EncError):
     """A known file is not available for download,  either because it is waiting
     for archiving or not yet generally available.
     """
-class BlacklistedFile(Error):
+class BlacklistedFile(EncError):
     """A known file has been blacklisted and should no longer be used."""
 
-class InvalidChunk(Error):
+class InvalidChunk(EncError):
     """The data chunk number requested from a file was invalid."""
     
-class BadFilenameError(Error):
+class BadFilenameError(EncError):
     """A filename uses characters prohibited to prevent SQL injection."""
 
-class BadListGlobError(Error):
+class BadListGlobError(EncError):
     """A list search pattern uses characters prohibited to prevent SQL injection."""
 
-class InvalidDatasetIds(Error):
+class InvalidDatasetIds(EncError):
     """Expected a list of dataset id strings but got something else."""
 
-class InvalidDateFormat(Error):
+class InvalidDateFormat(EncError):
     """Received a date with an invalid format."""
 
-class InvalidDateBasedContext(Error):
+class InvalidDateBasedContext(EncError):
     """Received a data based context with an invalid format."""
 
-class InvalidFileList(Error):
+class InvalidFileList(EncError):
     """Not a list of file names."""
 
-class InvalidFieldList(Error):
+class InvalidFieldList(EncError):
     """Not a list of database field names."""
 
 def check_known_file(filename):
@@ -119,9 +134,9 @@ def check_known_file(filename):
     check_filename(filename)
     blob = imodels.file_exists(filename)
     if blob is None:
-        raise UnknownFile("File '%s' is not known to CRDS."  % filename)
+        raise UnknownFile("File '{0}' is not known to CRDS.", filename)
     if not blob.available: 
-        raise UnavailableFile("File '%s' is not yet available."  % filename)
+        raise UnavailableFile("File '{0}' is not yet available.", filename)
     return blob
 
 def check_context(context, observatory=None):
@@ -134,16 +149,16 @@ def check_context(context, observatory=None):
         observatory = check_observatory(observatory)
     if not crds.config.is_mapping(context):  # this is for speed, to short circuit most checking
         if not crds.config.is_mapping_spec(context):  # this is for more clarity
-            raise UnknownContextError("Context parameter '%s' is not a .pmap, .imap, or .rmap file"
-                                      " or a valid date based context specification." % context)
+            raise UnknownContextError("Context parameter '{0}' is not a .pmap, .imap, or .rmap file"
+                                      " or a valid date based context specification.", context)
         context = _check_date_based_context(context, observatory)
     if config.observatory not in context:
-        raise MismatchedContextError(("Requested context '%s' doesn't match the observatory '%s'" + 
-                                      " supported by this server.   Switch servers or contexts.") %
-                                      (context, config.observatory))
+        raise MismatchedContextError("Requested context '{0}' doesn't match the observatory '{1}'" \
+                                     " supported by this server.   Switch servers or contexts.", 
+                                     context, config.observatory)
     _blob = check_known_file(context)
     if not crds.config.is_mapping(context):
-        raise UnknownContextError("Context parameter '%s' is not a known CRDS .pmap, .imap, or .rmap file." % context)
+        raise UnknownContextError("Context parameter '{0}' is not a known CRDS .pmap, .imap, or .rmap file.", context)
     return context
 
 def _check_date_based_context(context, observatory):
@@ -159,7 +174,8 @@ def _check_date_based_context(context, observatory):
         if match:
             obs_instr_kind = match.group("context")[:-1]
             parts = obs_instr_kind.split("-")
-            assert 1 <= len(parts) <= 3, "Invalid date based context [observatory-[instrument-[reftype]]] spec."
+            if not 1 <= len(parts) <= 3:
+                raise InvalidDateBasedContext("Invalid date based context [observatory-[instrument-[reftype]]] spec.")
             observatory = check_observatory(parts[0])
             instrument = parts[1] if len(parts) > 1 else None
             filekind = parts[2] if len(parts) > 2 else None
@@ -173,14 +189,14 @@ def _check_date_based_context(context, observatory):
         try:
             instrument = check_instrument(instrument)
         except Exception:
-            raise InvalidDateBasedContext("Bad instrument '%s' in CRDS date based context specification." % instrument)
+            raise InvalidDateBasedContext("Bad instrument '{0}' in CRDS date based context specification.", instrument)
         imap = pmap.get_imap(instrument)
         context = imap.name
         if filekind:
             try:
                 filekind = check_reftype(filekind)
             except Exception:
-                raise InvalidDateBasedContext("Bad reftype '%s' in  CRDS date based context specification." % filekind)
+                raise InvalidDateBasedContext("Bad reftype '{0}' in  CRDS date based context specification.", filekind)
             context = imap.get_rmap(filekind).name
     return context
 
@@ -190,7 +206,7 @@ def _pmap_from_date(date, observatory):
     try:
         return imodels.get_context_by_date(date=date, observatory=observatory)
     except Exception:
-        raise UnknownContextError("No CRDS context found corresponding to (prior to) date '%s'" % date)
+        raise UnknownContextError("No CRDS context found corresponding to (prior to) date '{0}'", date)
 
 def check_context_date(date):
     """Verify the format of simple context datetime string `date` and return a datetime.datetime object."""
@@ -199,26 +215,25 @@ def check_context_date(date):
             raise Exception("Forced date error")
         return timestamp.parse_date(date)
     except Exception:
-        raise InvalidDateBasedContext("Invalid context date/time format '%s' "
-                                      "should be YYYY-MM-DDTHH:MM:SS | edit | operational" % date)
+        raise InvalidDateBasedContext("Invalid context date/time format '{0}' " \
+                                      "should be YYYY-MM-DDTHH:MM:SS | edit | operational", date)
 
 def check_date(date):
     try:
         return timestamp.is_datetime(date.replace("T"," "))   # secure
     except:
-        raise InvalidDateFormat("Invalid date/time specification: " + repr(str(date)) + 
-                                " should be YYYY-MM-DDTHH:MM:SS")
+        raise InvalidDateFormat("Invalid date/time specification: '{0}' should be YYYY-MM-DDTHH:MM:SS", date) 
 
 def check_mapping(mapping):
     blob = check_known_file(mapping)
     if blob.type != "mapping":
-        raise UnknownMappingError("Mapping parameter '%s' is not a known CRDS mapping file." % mapping)
+        raise UnknownMappingError("Mapping parameter '{0}' is not a known CRDS mapping file.", mapping)
     return blob
     
 def check_reference(reference):
     blob = check_known_file(reference)
     if blob.type != "reference":
-        raise UnknownReferenceError("Reference parameter '%s' is not a known CRDS reference file." % reference)
+        raise UnknownReferenceError("Reference parameter '{0}' is not a known CRDS reference file.", reference)
     return blob
 
 def check_header(header):
@@ -226,25 +241,23 @@ def check_header(header):
         raise InvalidHeaderError("Header parameter is not a dictionary.")
     for key, value in header.items():
         if not isinstance(key, basestring) or not FITS_KEY_RE.match(key):
-            raise InvalidHeaderError("Bad key in header " + repr(key))
+            raise InvalidHeaderError("Bad key in header {0}", key)
         if not isinstance(value, (basestring, int, float, bool)) or not FITS_VAL_RE.match(value):
-            raise InvalidHeaderError("Bad value in header... not a str, int, float, or bool " + repr(value))
+            raise InvalidHeaderError("Bad value in header... not a str, int, float, or bool, or illegal str: '{0}'", value)
     return header
 
 def check_observatory(obs):
     obs = obs.lower()
     if obs != imodels.OBSERVATORY:
-        raise InvalidObservatoryError("Mismatch between requested observatory " + 
-                                      repr(obs) + " and server observatory " + 
-                                      repr(imodels.OBSERVATORY))
+        raise InvalidObservatoryError("Mismatch between requested observatory '{0}' and server observatory '{1}'", 
+                                      obs, imodels.OBSERVATORY)
     return obs
 
 def check_instrument(instr):
     instr = instr.lower()
     if not isinstance(instr, basestring) or not INSTRUMENT_RE.match(instr) or instr not in imodels.INSTRUMENTS:
-        raise InvalidInstrumentError("Mismatch between requested instrument " + 
-                                     repr(instr) + " and server instruments " + 
-                                     repr(imodels.INSTRUMENTS))
+        raise InvalidInstrumentError("Mismatch between requested instrument '{0}' and server instruments '{1}'", 
+                                     instr, imodels.INSTRUMENTS)
     return instr
 
 def check_reftypes(reftypes):
@@ -258,10 +271,10 @@ def check_reftypes(reftypes):
 
 def check_reftype(reftype):
     if not isinstance(reftype, basestring):
-        raise InvalidReftypesError("Non-string reftype: " + repr(reftype))
+        raise InvalidReftypesError("Non-string reftype: '{0}'", reftype)
     reftype = reftype.lower()
     if reftype not in imodels.FILEKINDS:
-        raise InvalidReftypesError("Reftype '%s' is not a known reference type." % imodels.FILEKINDS)
+        raise InvalidReftypesError("Reftype '{0}' is not a known reference type.", imodels.FILEKINDS)
     return reftype
 
 def check_dataset_ids(datasets):
@@ -279,11 +292,11 @@ def check_header_map(header_map):
         raise InvalidDatasetIds("Expected object mapping dataset ids to headers: { dataset_id : { header } }.")
     for dataset, header in header_map.items():
         if not isinstance(dataset, basestring) or (not DATASET_ID_RE.match(dataset) and not FILE_RE.match(dataset)):
-            raise InvalidDatasetIds("Bad dataset id: " + repr(dataset))
+            raise InvalidDatasetIds("Bad dataset id: '{0}'", dataset)
         try:
             check_header(header)
         except Exception as exc:
-            raise InvalidHeaderError("Invalid header at dataset id '{}' : '{}'".format(dataset, str(exc)))
+            raise InvalidHeaderError("Invalid header at dataset id '{0}' : '{1}'", dataset, str(exc))
     return header_map
 
 def check_file_list(files):
@@ -312,7 +325,7 @@ def list_mappings(request, observatory, glob_pattern):
         observatory = config.observatory
     check_observatory(observatory)
     if not re.match(LIST_GLOB_RE, glob_pattern):
-        raise BadListGlobError("Illegal glob pattern, / not permitted '%s'" % glob_pattern)
+        raise BadListGlobError("Illegal glob pattern, not permitted '{0}'", glob_pattern)
     
     # Check if files en route to archive already got there.
     imodels.update_delivery_status()
@@ -342,8 +355,8 @@ def get_best_references_by_ids(request, context, dataset_ids, reftypes):
     dataset_ids = check_dataset_ids(dataset_ids)
     reftypes = check_reftypes(reftypes)
     pmap = rmap.get_cached_mapping(context)
-    assert len(dataset_ids) <= MAX_DATASETS_PER_CALL, \
-        "Get best references by ids limited to <= {} datasets per call.".format(MAX_DATASETS_PER_CALL)
+    if not len(dataset_ids) <= MAX_DATASETS_PER_CALL:
+        raise InvalidDatasetIds("Get best references by ids limited to <= '{0}' datasets per call.", MAX_DATASETS_PER_CALL)
     headers = database.get_dataset_headers_by_id(dataset_ids=dataset_ids, observatory=pmap.observatory)
     result = {}
     for dataset_id in dataset_ids:
@@ -393,7 +406,7 @@ def get_file_chunk(request, context, filename, chunk):
     if not isinstance(chunk, (int, float, basestring)) or math.floor(int(chunk)) != math.floor(chunk):
         raise InvalidChunk("the specified chunk must be an integer index.")
     if not (0 <= chunk < chunks):
-        raise InvalidChunk("the specified data chunk " + repr(chunk) + " is invalid.")
+        raise InvalidChunk("the specified data chunk '{0}' is out of range.", chunk)
     with open(blob.pathname, "rb") as infile:
         infile.seek(chunk*CRDS_JSONRPC_CHUNK_SIZE)
         data = infile.read(CRDS_JSONRPC_CHUNK_SIZE)
