b7100eb75586904569bc47cbd1403dc9fa5c9af3
stsci_jmiller <stsci_jmiller@stsci.edu>
2014-03-03 13:45:27 +0000

    Modified manage.py for Django-1.6
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@1330 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/manage.py
+++ sources/manage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import sys
+import os
 import pdb
+
+from django.core.management import execute_from_command_line
 
 if "--pdb" in sys.argv:
     PDB = True
@@ -8,17 +11,12 @@ if "--pdb" in sys.argv:
 else:
     PDB = False
 
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
 
 if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crds.server.settings")
+
     if PDB:
-        pdb.runctx("execute_manager(settings)", locals(), globals())
+        pdb.runctx("execute_from_command_line(sys.argv)", locals(), globals())
     else:
-        execute_manager(settings)
+        execute_from_command_line(sys.argv)
 
