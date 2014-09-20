9aa7517c1e016a769dd10ae328884eefb314222c
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2014-09-20 11:30:57 +0000

    Enabled context generation for super user in unlocked mode.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@1716 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/interactive/views.py
+++ sources/interactive/views.py
@@ -1470,7 +1470,8 @@ def submit_files_post(request, crds_filetype):
     _remove_dir, uploaded_files = get_files(request)
     locked_instrument = get_locked_instrument(request)
 
-    assert not generate_contexts or locked_instrument,  "Can't generate contexts in unlocked mode."
+    assert not generate_contexts or locked_instrument or request.user.is_superuser,  \
+        "Can't generate contexts in unlocked mode."
     
     jpoll_handler = jpoll_views.get_jpoll_handler(request)
     
