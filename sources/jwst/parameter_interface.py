e045bca3667b0dd55ed58368c96682b51e5b905d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-14 20:45:31 +0000

    Added prototype interface to Archive CRDCS parameters interface.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@2054 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

---
+++ sources/jwst/parameter_interface.py
@@ -0,0 +1,70 @@
+from crds.server.jwst import restful
+from crds.server import config
+from crds import log
+
+BASE_URL = "http://jwdmsdevvm4:8888/crds"
+
+# ================================================================================================== 
+
+class SimpleIntArchiveService(restful.GetService):
+   
+    base_url = BASE_URL
+
+    def format_result(self, result):
+        varname = self.service_name[0].capitalize() + self.service_name[1:]
+        return int(result[varname])
+
+max_header_block_size = SimpleIntArchiveService("maxHeaderBlockSize")
+
+datasets_count = SimpleIntArchiveService("datasetsCount")
+
+# ================================================================================================== 
+
+class BatchStringListService(restful.GetService):
+
+    base_url = BASE_URL
+
+    def format_result(self, result):
+        return [str(name) for name in list(result)]
+
+datasets = BatchStringListService("datasets")
+
+# ================================================================================================== 
+
+def get_dataset_ids(instrument, datasets_since=None):
+    if datasets_since:
+        datasets_since = datasets_since.replace(" ","T").split(".")[0]
+    expected_count = datasets_count(instrument=instrument, minDate=datasets_since)
+    total_ids = []
+    result = ["throwaway starter result"]
+    batch = 0
+    while len(total_ids) < expected_count and result:
+        result = datasets(instrument=instrument, minDate=datasets_since, batchNum=batch)
+        batch += 1
+        total_ids += result
+        log.verbose("get_dataset_ids:", len(result), "/", len(total_ids), "/", expected_count)
+    return total_ids
+
+# ================================================================================================== 
+
+class ArchivePostService(restful.PostService):
+    base_url = BASE_URL
+
+headers = ArchivePostService("headers")
+
+def get_header_block(dataset_ids, matching_parameters):
+    return headers(CRDSHeaderQueryInput=dict(
+            datasetIds=dataset_ids, parameters=matching_parameters))
+
+def get_dataset_headers_by_id(dataset_ids, matching_parameters):
+    max_headers = max_header_block_size()
+    total_headers = {}
+    for i in range(0, len(dataset_ids), max_headers):
+        results = get_header_block(
+            dataset_ids[i:min(i+max_headers, len(dataset_ids))], matching_parameters)
+        total_headers.update(results)
+    for did in dataset_ids:
+        if did not in total_headers:
+            total_headers[did] = "UNDEFINED"
+    return total_headers
+
---
+++ sources/jwst/restful.py
@@ -0,0 +1,101 @@
+"""
+This module defines base classes for implementing arbitrary REST-ful HTTP queries.
+"""
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+# =====================================================================
+
+import sys
+import uuid
+import json
+import time
+import os
+import ssl
+
+if sys.version_info < (3, 0, 0):
+    import HTMLParser as parser_mod
+    from urllib import urlencode
+    from urllib2 import urlopen, Request
+else:
+    import html.parser as parser_mod
+    from urllib.request import urlopen, Request
+
+# =====================================================================
+
+from crds import log, config, python23
+from crds.client import proxy
+
+from crds.exceptions import ServiceError
+
+# =====================================================================
+
+PARSER = parser_mod.HTMLParser()
+
+# =====================================================================
+
+class GetService(object):
+
+    base_url = "abstract,  must subclass and define for actual service class."
+    kind = "GET"
+
+    def __init__(self, service_name):
+        self.service_name = service_name
+        self.service_url = self.base_url + "/" + service_name
+
+    def __call__(self, **named_parameters):
+        log.verbose(self.kind, repr(self.service_url), "with", repr(named_parameters))
+        formatted_parameters = self.format_parameters(named_parameters)
+        response = self.get_response(formatted_parameters)
+        log.verbose("responded:", repr(response))
+        result = self.get_and_decode_result(response)
+        log.verbose("decoded:", repr(result))
+        formatted = self.format_result(result)
+        log.verbose("returning:", formatted)
+        return formatted
+
+    def get_response(self, parameters):
+        param_str = self.format_parameters(parameters)
+        url = self.service_url if not param_str else self.service_url + "?" + param_str
+        return urlopen(url)
+
+    def format_parameters(self, parameters):
+        if isinstance(parameters, dict):
+            parameters = parameters.items()
+        formatted = "&".join([name + "="+ str(value) for (name, value) in parameters
+                              if value is not None])
+        return formatted
+
+    def get_and_decode_result(self, response):
+        page = response.read()
+        decoded = page.decode("utf-8")
+        result = json.loads(decoded)
+        return result
+
+    def format_result(self, result):
+        return result
+
+# =====================================================================
+
+class PostService(GetService):
+
+    kind = "POST"
+
+    def get_response(self, formatted_parameters):    # full override
+        headers = {
+            "Content-type" : "application/json",
+            }
+        request = Request(self.service_url)
+        request.add_header('Content-Type', 'application/json')
+        return urlopen(request, formatted_parameters)
+
+    def format_parameters(self, named_parameters):
+        parameters = json.dumps(named_parameters)
+        if not isinstance(parameters, bytes):  # Python-3
+            parameters = parameters.encode("utf-8")
+        return parameters
+
+    def format_result(self, result):
+        return result
+
