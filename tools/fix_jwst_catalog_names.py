f9bd4ba6f5f4d8bd90930ccca1188c695588bf1d
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2015-08-19 16:35:38 +0000

    Fixed filename in AuditBlobs for renamed opus_\d+_i.cat files to jwst_\d+.cat files.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@2065 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- tools/fix_jwst_catalog_names.py
+++ tools/fix_jwst_catalog_names.py
@@ -17,16 +17,17 @@ def fix_file_blobs():
         blob.catalog_link = fix_catalog_name(blob.catalog_link)
         blob.save()
 
-def fix_catalog_name(name):
-    return re.sub(r"opus_(\d+)_i.cat", r"jwst_\1.cat", name)
-
 def fix_audit_blobs():
     for blob in models.AuditBlob.objects.all():
+        blob.filename = fix_catalog_name(blob.filename)
         blob.details = fix_details(blob.details)
         blob.save()
 
 def fix_details(details_text):
     return fix_catalog_name(details_text)  # works because re.sub does many replacements
+
+def fix_catalog_name(name):
+    return re.sub(r"opus_(\d+)_i.cat", r"jwst_\1.cat", name)
 
 def main():
     import django
