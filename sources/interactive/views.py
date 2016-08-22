5f4a9d81f99f6a8b3159c8c7c1945e1598645f3b
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-08-22 20:32:00 +0000

    Formatting improvements for file browser details page.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@2540 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/interactive/views.py
+++ sources/interactive/views.py
@@ -1814,7 +1814,7 @@ def browsify_file(filename, browsed_file):
         file_contents = browsifier(filename, browsed_file)
     except Exception, exc:
         log.error("browsify_file failed: ", str(exc))
-        file_contents = "<pre class='program'>Content display for '{}' not available</pre>".format(
+        file_contents = "<pre class='error'>Content display for '{}' not available</pre>".format(
             os.path.basename(filename))
     return file_contents
 
@@ -1848,7 +1848,7 @@ def browsify_header(filename, browsed_file):
         output += "<table border='1'>\n"
         for key, value in sorted(header.items()):
             if value != "UNDEFINED":
-                output += format_html("<tr><td class='label'>{0}</td><td>{1}</td></tr>\n", key, value)
+                output += format_html("<tr><td class='tdlabel'>{0}</td><td>{1}</td></tr>\n", key, value)
         output += "</table>\n"
     else:
         output = format_html("<p class='error'>File header unavailable for '{0}'</p>", browsed_file)
@@ -1863,13 +1863,17 @@ def browsify_finfo(filename, browsed_file):
     except Exception, exc:
         output += format_html("<p class='error'>FITS info unavailable: '{0}'</p>", exc)
     else:
-        output += "<b>FITS Info</b>\n"
-        output += "<pre>\n"
+        output += "<p><b>FITS Info</b></p>\n"
+        output += "<br/>\n"
+        output += "<div class='simple_white'>\n"
+        output += "<pre class='simple_white'>\n"
         lines = fits_info.split("\n")
         if lines[0].lower().startswith("filename"):
             lines = lines[1:]
         output += format_html_join("\n", "{0}", ((line,) for line in lines))
         output += "</pre>\n"
+        output += "</div>\n"
+        output += "<br/>\n"
     return output
 
 def browsify_asdf(filename, browsed_file):
@@ -1878,7 +1882,10 @@ def browsify_asdf(filename, browsed_file):
     with log.error_on_exception("Can't extract ASDF tree from", srepr(browsed_file)):
         tree = get_asdf_tree(browsed_file)
         info += "<b>ASDF Tree</b>\n"
+        info += "<br/>+\n"
+        info += "<div class='simple_white'>\n"
         info += "<pre>\n" + tree + "\n</pre>\n"
+        info += "</div>\n"
     return info
 
 def get_asdf_tree(filepath):
