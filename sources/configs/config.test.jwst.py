aefbc3eb5ea41bb17d4fc613d5c3736a2e64f489
jmiller@stsci.edu <jmiller@stsci.edu@stsci.edu>
2016-12-19 18:30:50 +0000

    Removed JWST D-string I&T archive parameter service references from non-D-string servers.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/crds_server/trunk@2686 37088bc5-bd3f-4c5b-b9de-119bb1e8f7e3

--- sources/configs/config.test.jwst.py
+++ sources/configs/config.test.jwst.py
@@ -58,8 +58,8 @@ FORCE_REMOTE_MODE = False
 
 # ARCHIVE_PARAMETER_SERVICE_URL = "https://jwdmsdevvm4.stsci.edu:8888/crds"
 # ARCHIVE_PARAMETER_SERVICE_URL = "https://undefined-archive-parameter-service.stsci.edu:8888/crds"
-ARCHIVE_PARAMETER_SERVICE_URL = "http://iljwdmsdarcv.stsci.edu:8888/crds"
+# ARCHIVE_PARAMETER_SERVICE_URL = "http://iljwdmsdarcv.stsci.edu:8888/crds"
 
-CRDS_MOCK_ARCHIVE_PARAMETERS = "jwst-b7-params-dit-20160907.json"
+CRDS_MOCK_ARCHIVE_PARAMETERS = "jwst-b7-params-dit-20161209.json"
 
 # CRDS_STATUS_TO_ADDRESSES = ["crds-servers@stsci.edu", "redcat@stsci.edu"]
