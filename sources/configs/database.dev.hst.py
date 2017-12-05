d11143d84f7f788857588d3a16c65f7d403c183c
CRDS - Todd Miller <crds@dlhstcrdsv1.stsci.edu>
2017-12-05 10:00:20 -0500

    Added CRDS change request form for internal tracking purposes,  forms will be maintained on the JWST OPS server only and initially managed through the admin interface.

--- sources/configs/database.dev.hst.py
+++ sources/configs/database.dev.hst.py
@@ -11,7 +11,8 @@ DATABASES = {
         'PORT': '23306',                      # Set to empty string for default. Not used with sqlite3.
 
         "OPTIONS": {
-           'init_command': 'SET default_storage_engine=INNODB;',
+           'init_command': 'SET default_storage_engine=INNODB; SET sql_mode=STRICT_TRANS_TABLES',
+           # 'init_command': 'SET default_storage_engine=INNODB',
         },
     }
 }
