da4d4832226988b94974810e76efd717f7132e57
CRDS - Todd Miller <crds@tljwcrds.stsci.edu>
2018-02-18 12:19:43 -0500

    Factored out dev, test DB info

--- sources/configs/database.test.jwst.py
+++ sources/configs/database.test.jwst.py
@@ -1,12 +1,15 @@
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
+
+DBWORD = open("/crds/data1/database/dev_test_django_jwst.dat").read().strip()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'crds_jwst_test',
         'USER': 'jwstcrds',                      # Not used with sqlite3.
-        'PASSWORD': 'uj297rWzaNyk',                  # Not used with sqlite3.
+        'PASSWORD': DBWORD,                  # Not used with sqlite3.
         'HOST': 'goldtst.stsci.edu',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '23306',                      # Set to empty string for default. Not used with sqlite3.
 
