3957384f0dba3966bfb020fa35e7a6f25589fd6b
CRDS - Todd Miller <crds@tljwcrds.stsci.edu>
2018-02-18 12:20:40 -0500

    Updated version to 7.2.0

--- sources/setup.py
+++ sources/setup.py
@@ -107,7 +107,7 @@ PACKAGE_DATA[''].extend(static_files)
 # DATA_FILES = [("", static_files)]
 
 setup(name="crds.server",
-      version= "7.1.0",
+      version= "7.2.0",
       description="Python based Calibration Reference Data System,  best reference file web service",
       author="Todd Miller",
       author_email="jmiller@stsci.edu",
