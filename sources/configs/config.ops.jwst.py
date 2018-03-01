9f083296a409ea120ffc274299c00033e22aa5c2
CRDS - Todd Miller <crds@iljwdmsccrds.stsci.edu>
2018-03-01 09:36:53 -0500

    Updated JWST OPS mock parameters from C-string.

--- sources/configs/config.ops.jwst.py
+++ sources/configs/config.ops.jwst.py
@@ -64,7 +64,7 @@ FORCE_REMOTE_MODE = False
 # ARCHIVE_PARAMETER_SERVICE_URL = "http://iljwdmscarcv.stsci.edu:8888/crds"
 # ARCHIVE_PARAMETER_SERVICE_URL = "https://dljwdms5v1.stsci.edu:8888/crds"
 ARCHIVE_PARAMETER_SERVICE_URL = "https://undefined-archive-parameter-service.stsci.edu:8888/crds"
-CRDS_MOCK_ARCHIVE_PARAMETERS = "jwst-b7.1-iljwdmsbarcv1-2017-11-14.json"
+CRDS_MOCK_ARCHIVE_PARAMETERS = "jwst-b7.1.x-iljwdmscarc1-2018-03-01.json"
 
 CRDS_STATUS_TO_ADDRESSES = ["crds-servers@stsci.edu", "redcat@stsci.edu"]
 
