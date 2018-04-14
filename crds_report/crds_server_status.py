2704440a7497db75ed5993ed447c4d06c1e3988a
CRDS - Todd Miller <crds@dljwcrds.stsci.edu>
2018-04-14 11:02:20 -0400

    CRDS report baseline, switched to OPS servers.

--- crds_report/crds_server_status.py
+++ crds_report/crds_server_status.py
@@ -21,33 +21,53 @@ import crds_rst
 
 DEFAULT_SINCE_DATE = (datetime.datetime.now() + datetime.timedelta(days=-14)).isoformat().split(".")[0]
 
-DEFAULT_FIELDS = "date,context,status,description".split(",")
-
 # ==============================================================================
 
 class ServerStatus(object):
-    def __init__(self, observatory, usecase, url, 
-                 since_date=DEFAULT_SINCE_DATE,
-                 delivery_fields=DEFAULT_FIELDS):
+    def __init__(self, observatory, usecase, url, since_date=DEFAULT_SINCE_DATE):
         self.observatory = observatory
         self.usecase = usecase
         self.url = url
         self.since_date = since_date
-        self.delivery_fields = delivery_fields
+        self.link_defs = []
         api.set_crds_server(self.url)
         os.environ["CRDS_OBSERVATORY"] = observatory
 
     def to_rst(self):
         return self.title_rst() + "\n" + \
-            self.context_rst() + "\n" + \
             self.delivery_rst() + "\n" + \
-            self.links_rst() + "\n"
+            self.context_rst() + "\n" + \
+            self.support_rst() + "\n" + \
+            self.link_defs_rst()
+
+    # ---------------------------------------------------------------------
+
+    def add_context_links(self, index, rows):
+        rst_rows = []
+        for row in rows:
+            context_name = row[index]
+            context_use_rst = crds_rst.link_use_rst(context_name)
+            row = row[:index] + (context_use_rst,) + row[index+1:]
+            rst_rows.append(row)
+            context_link = self.url + "/context_table/" + context_name
+            self.add_link_def(context_name, context_link)
+        return rst_rows
+
+    def add_link_def(self, name, url):
+        link_def = crds_rst.link_def_rst(name, url)
+        if link_def not in self.link_defs:
+            self.link_defs.append(link_def)
+
+    def link_defs_rst(self):
+        return "\n".join(self.link_defs) + "\n"
+
+    # ---------------------------------------------------------------------
 
     def title_rst(self):
-        title = self.observatory.upper()
+        title = "CRDS " + self.observatory.upper() + " " + self.usecase.upper()
         return crds_rst.underline(title, "-")
 
-    def links_rst(self):
+    def support_rst(self):
         OBS = self.observatory.upper()
         URL = self.url
         return f"""
@@ -65,6 +85,8 @@ Links
 .. _`{OBS} All Contexts`:  {URL}/display_all_contexts/
 """
 
+    # ---------------------------------------------------------------------
+
     def context_info(self):
         names = ["Location", "Context"]
         rows = [
@@ -77,17 +99,13 @@ Links
 
     def context_rst(self):
         names, rows = self.context_info()
-        rst_rows = []
-        link_defs = ""
-        for row in rows:
-            rst_rows.append((row[0], crds_rst.link_use_rst(row[1])))
-            link_url = self.url + "/context_table/" + row[1]
-            link_def = crds_rst.link_def_rst(row[1], link_url)
-            link_defs += link_def + "\n"
+        rst_rows = self.add_context_links(1, rows)
         table = crds_rst.CrdsTable("Context Settings", names, rst_rows, format=("+","="))
-        rst = table.to_rst() + "\n"
-        rst += link_defs
-        return rst
+        return table.to_rst() + "\n"
+
+    # ---------------------------------------------------------------------
+
+    delivery_fields = "date,context,status,description".split(",")
 
     def get_delivery_status(self):
         """Return a list of delivery status dictionaries for deliveries which occurred
@@ -95,21 +113,19 @@ Links
         """
         return api.S.get_delivery_status(self.since_date)
 
-    def delivery_rst(self):
+    def delivery_info(self):
         try:
             deliveries = self.get_delivery_status()
         except Exception as exc:
             log.error("Failed obtaining delivery status:", str(exc))
-            return ""
+            return (),()
         deliveries = self.clean_deliveries(deliveries)
         rows = []
         for delivery in deliveries:
             row = self.get_row(delivery)
             rows.append(row)
-        title = "Deliveries"
         names = [name.capitalize() for name in self.delivery_fields]
-        table = crds_rst.CrdsTable(title, names, rows, format=("+","="))
-        return table.to_rst()
+        return names, rows
 
     def get_row(self, delivery):
         return tuple(delivery[name] for name in self.delivery_fields)
@@ -120,9 +136,18 @@ Links
             clean = dict(delivery)
             clean["date"] = delivery["date"].split(".")[0]
             clean["context"] = [ file for file in delivery["files"]
-                                         if file.endswith(".pmap")][0]
+                                 if file.endswith(".pmap")][0]
         cleaned.append(clean)
         return cleaned
+
+    def delivery_rst(self):
+        names, rows = self.delivery_info()
+        if not names:
+            return ""
+        rst_rows = self.add_context_links(1, rows)
+        table = crds_rst.CrdsTable(
+            "Deliveries", names, rst_rows, format=("+","="))
+        return table.to_rst() + "\n"
 
 # ==============================================================================
 
