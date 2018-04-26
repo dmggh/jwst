a6b1982d58bb63c8fd738912f0e79c8534aed0c1
CRDS - Todd Miller <crds@dljwcrds.stsci.edu>
2018-04-26 10:37:45 -0400

    More polished crds_report refactoring for monthlies,  still incomplete.

--- crds_report/crds_underline.py
+++ crds_report/crds_underline.py
@@ -7,10 +7,10 @@ import crds_rst
 # ==================================================
 
 def main(title, underline_char="^"):
-    print(crds.rst.underline(title, underline_char))
+    print(crds_rst.underline(title, underline_char))
 
 # ==================================================
-e
+
 if __name__ == "__main__":
     main(*sys.argv[1:])
 
