76102196c4eee9a7abc0940757d5cccd1b5995af
CRDS - Todd Miller <crds@dljwcrds.stsci.edu>
2018-12-14 10:08:07 -0500

    Eliminated unclosed file warnings using with open() in database setup for DEV, TEST, OPS, B-string, C-string. Deleted config and database files for B5IT, B6IT, D-string.

--- sources/configs/database.bit.jwst.py
+++ sources/configs/database.bit.jwst.py
@@ -1,11 +1,12 @@
-DBWORD = open("/crds/data1/database/production_django_jwst.dat").read().strip()
+with open("/crds/data1/database/production_django_jwst.dat") as db_file:
+    DB_WORD = db_file.read().strip()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'crds_jwst_bit',
         'USER': 'jwstcrds',                      # Not used with sqlite3.
-        'PASSWORD': DBWORD,              # Not used with sqlite3.
+        'PASSWORD': DB_WORD,              # Not used with sqlite3.
         'HOST': 'iljwdmsbcrdsdb.stsci.edu',             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
         "OPTIONS": {
--- sources/configs/database.test.hst.py
+++ sources/configs/database.test.hst.py
@@ -1,11 +1,12 @@
-DBWORD = open("/crds/data1/database/dev_test_django_hst.dat").read().strip()
+with open("/crds/data1/database/dev_test_django_hst.dat") as db_file:
+    DB_WORD = db_file.read().strip()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'crds_hst_test',
         'USER': 'hstcrds',                      # Not used with sqlite3.
-        'PASSWORD': DBWORD,                  # Not used with sqlite3.
+        'PASSWORD': DB_WORD,                  # Not used with sqlite3.
         'HOST': 'goldtst.stsci.edu',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '23306',                      # Set to empty string for default. Not used with sqlite3.
 
